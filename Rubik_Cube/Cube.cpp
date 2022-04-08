8e309f265382d3c6fb144a2719a62bcce6ae5cda
Bansai3 <931307@bk.ru>
2022-04-08 23:52:09 +0300

    Matrix turn Mixing

--- Rubik_Cube/Cube.cpp
+++ Rubik_Cube/Cube.cpp
@@ -17,8 +17,6 @@ std::ostream& operator<<(std::ostream& out, Cube& cube)
 		}
 		std::cout << std::endl;
 	}
-
-	std::cout << std::endl;
 	 
 	for (int i = 0; i < 3; i++)
 	{
@@ -51,8 +49,6 @@ std::ostream& operator<<(std::ostream& out, Cube& cube)
 		std::cout << std::endl;
 	}
 
-	std::cout << std::endl;
-
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -82,7 +78,7 @@ void Cube::load_from_file(std::string filename)
 		for (int k = 0; k < 3; k++)
 		{
 			fin >> colour;
-			cube[top][i][k] = colour;
+			cube[Top][i][k] = colour;
 		}
 	}
 
@@ -91,25 +87,25 @@ void Cube::load_from_file(std::string filename)
 		for (int j = 0; j < 3; j++)
 		{
 			fin >> colour;
-			cube[left][i][j] = colour;
+			cube[Left][i][j] = colour;
 		}
 
 		for (int j = 0; j < 3; j++)
 		{
 			fin >> colour;
-			cube[forward][i][j] = colour;
+			cube[Forward][i][j] = colour;
 		}
 
 		for (int j = 0; j < 3; j++)
 		{
 			fin >> colour;
-			cube[right][i][j] = colour;
+			cube[Right][i][j] = colour;
 		}
 
 		for (int j = 0; j < 3; j++)
 		{
 			fin >> colour;
-			cube[back][i][j] = colour;
+			cube[Back][i][j] = colour;
 		}
 	}
 
@@ -118,9 +114,297 @@ void Cube::load_from_file(std::string filename)
 		for (int k = 0; k < 3; k++)
 		{
 			fin >> colour;
-			cube[botton][i][k] = colour;
+			cube[Botton][i][k] = colour;
 		}
 	}
+}
+
+
+
+void Cube::turn_matrix(int facet, bool cw)
+{
+	char facet_matrix[3][3];
+	char side_col_matrix[3][4];
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			facet_matrix[i][j] = cube[facet][i][j];
+		}
+	}
+
+	if (cw)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				cube[facet][j][2 - i] = facet_matrix[i][j];
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				cube[facet][2 - j][i] = facet_matrix[i][j];
+			}
+		}
+	}
+
+	if (facet == Forward)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			side_col_matrix[j][0] = cube[Top][2][j];
+			side_col_matrix[j][1] = cube[Right][j][0];
+			side_col_matrix[j][2] = cube[Botton][0][j];
+			side_col_matrix[j][3] = cube[Left][j][2];
+		}
+		if (cw)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Right][i][0] = side_col_matrix[i][0];
+				cube[Botton][0][i] = side_col_matrix[2 - i][1];
+				cube[Left][i][2] = side_col_matrix[i][2];
+				cube[Top][2][i] = side_col_matrix[2 - i][3];
+			}
+		}
+		else
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Right][i][0] = side_col_matrix[2 - i][2];
+				cube[Botton][0][i] = side_col_matrix[i][3];
+				cube[Left][i][2] = side_col_matrix[2 - i][0];
+				cube[Top][2][i] = side_col_matrix[i][1];
+			}
+		}
+	}
+	else if (facet == Back)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			side_col_matrix[j][0] = cube[Top][0][j];
+			side_col_matrix[j][1] = cube[Right][j][2];
+			side_col_matrix[j][2] = cube[Botton][2][j];
+			side_col_matrix[j][3] = cube[Left][j][0];
+		}
+		if (cw)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Right][i][2] = side_col_matrix[2 - i][2];
+				cube[Botton][2][i] = side_col_matrix[i][3];
+				cube[Left][i][0] = side_col_matrix[2 - i][0];
+				cube[Top][0][i] = side_col_matrix[i][1];
+			}
+		}
+		else
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Right][i][2] = side_col_matrix[i][0];
+				cube[Botton][2][i] = side_col_matrix[2 - i][1];
+				cube[Left][i][0] = side_col_matrix[i][2];
+				cube[Top][0][i] = side_col_matrix[2 - i][3];
+			}
+		}
+	}
+	else if (facet == Right)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			side_col_matrix[j][0] = cube[Top][j][2];
+			side_col_matrix[j][1] = cube[Forward][j][2];
+			side_col_matrix[j][2] = cube[Botton][j][2];
+			side_col_matrix[j][3] = cube[Back][j][0];
+		}
+		if (cw)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Top][i][2] = side_col_matrix[i][1];
+				cube[Forward][i][2] = side_col_matrix[i][2];
+				cube[Botton][i][2] = side_col_matrix[2 - i][3];
+				cube[Back][i][0] = side_col_matrix[2 - i][0];
+			}
+		}
+		else
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Top][i][2] = side_col_matrix[2 - i][3];
+				cube[Forward][i][2] = side_col_matrix[i][0];
+				cube[Botton][i][2] = side_col_matrix[i][1];
+				cube[Back][i][0] = side_col_matrix[2 - i][2];
+			}
+		}
+	}
+	else if (facet == Left)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			side_col_matrix[j][0] = cube[Top][j][0];
+			side_col_matrix[j][1] = cube[Forward][j][0];
+			side_col_matrix[j][2] = cube[Botton][j][0];
+			side_col_matrix[j][3] = cube[Back][j][2];
+		}
+		if (cw)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Top][i][0] = side_col_matrix[2 - i][3];
+				cube[Forward][i][0] = side_col_matrix[i][0];
+				cube[Botton][i][0] = side_col_matrix[i][1];
+				cube[Back][i][2] = side_col_matrix[2 - i][2];
+			}
+		}
+		else
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Top][i][0] = side_col_matrix[i][1];
+				cube[Forward][i][0] = side_col_matrix[2 - i][2];
+				cube[Botton][i][0] = side_col_matrix[2 - i][3];
+				cube[Back][i][2] = side_col_matrix[2 - i][0];
+			}
+		}
+	}
+	else if (facet == Top)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			side_col_matrix[j][0] = cube[Back][0][j];
+			side_col_matrix[j][1] = cube[Right][0][j];
+			side_col_matrix[j][2] = cube[Forward][0][j];
+			side_col_matrix[j][3] = cube[Left][0][j];
+		}
+		if (cw)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Back][0][i] = side_col_matrix[i][3];
+				cube[Right][0][i] = side_col_matrix[i][0];
+				cube[Forward][0][i] = side_col_matrix[i][1];
+				cube[Left][0][i] = side_col_matrix[i][2];
+			}
+		}
+		else
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Back][0][i] = side_col_matrix[i][1];
+				cube[Right][0][i] = side_col_matrix[i][2];
+				cube[Forward][0][i] = side_col_matrix[i][3];
+				cube[Left][0][i] = side_col_matrix[i][0];
+			}
+		}
+	}
+	else if (facet == Botton)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			side_col_matrix[j][0] = cube[Back][2][j];
+			side_col_matrix[j][1] = cube[Right][2][j];
+			side_col_matrix[j][2] = cube[Forward][2][j];
+			side_col_matrix[j][3] = cube[Left][2][j];
+		}
+		if (cw)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Back][2][i] = side_col_matrix[i][1];
+				cube[Right][2][i] = side_col_matrix[i][2];
+				cube[Forward][2][i] = side_col_matrix[i][3];
+				cube[Left][2][i] = side_col_matrix[i][0];
+			}
+		}
+		else
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cube[Back][2][i] = side_col_matrix[i][3];
+				cube[Right][2][i] = side_col_matrix[i][0];
+				cube[Forward][2][i] = side_col_matrix[i][1];
+				cube[Left][2][i] = side_col_matrix[i][2];
+			}
+		}
+	}
+}
+
+void Cube::F(bool cw)
+{
+	turn_matrix(Forward, cw);
+}
+
+void Cube::B(bool cw)
+{
+	turn_matrix(Back, cw);
+}
+
+void Cube::L(bool cw)
+{
+	turn_matrix(Left, cw);
+}
+
+void Cube::R(bool cw)
+{
+	turn_matrix(Right, cw);
+}
+
+void Cube::U(bool cw)
+{
+	turn_matrix(Top, cw);
+}
+
+void Cube::D(bool cw)
+{
+	turn_matrix(Botton, cw);
+}
+
+void Cube::Mixing()
+{
+	srand(time(0));
+	for (int i = 0; i < 15; i++)
+	{
+		int rand_number = rand() % 6;
+		if (rand_number == 0)
+		{
+			F();
+			std::cout << "F";
+		}
+		if (rand_number == 1)
+		{
+			B();
+			std::cout << "B";
+		}
+		if (rand_number == 2)
+		{
+			L();
+			std::cout << "L";
+		}
+		if (rand_number == 3)
+		{
+			R();
+			std::cout << "R";
+		}
+		if (rand_number == 4)
+		{
+			U();
+			std::cout << "U";
+		}
+		if (rand_number == 5)
+		{
+			D();
+			std::cout << "D";
+		}
+	}
+	std::cout << std::endl;
 }
 
 Cube::Cube()
@@ -139,5 +423,24 @@ Cube::Cube()
 
 bool Cube::checkState()
 {
-	return false;
+	for (int i = 0; i < 6; i+=2)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			if (cube[i][1][1] == colors[j])
+			{
+				if (j % 2 == 0)
+				{
+					if (cube[i + 1][1][1] != colors[j + 1])
+						return false;
+				}
+				else
+				{
+					if (cube[i + 1][1][1] != colors[j - 1])
+						return false;
+				}
+			}
+		}
+	}
+
 }
--- Rubik_Cube/Source.cpp
+++ Rubik_Cube/Source.cpp
@@ -6,6 +6,7 @@ int main()
 	std::string filename = "cube_state.in";
 	Cube cube;
 	cube.load_from_file(filename);
+	cube.Mixing();
 	std::cout << cube;
 	return 0;
 }
